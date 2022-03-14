10f415f6efadadea8a96aad8603880d7092b689b
hiren patel <89856028+ghostediting@users.noreply.github.com>
2022-03-14 16:56:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,7 +34,7 @@ async def start(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await client.send_sticker(chat_id=message.chat.id, sticker='CAACAgUAAxkBAAJ1k2IfDr7l8Hat1d-s1mnbBEsFXZWJAAL5BAACAdz5VOVw2x38ZZJ1HgQ', reply_markup=reply_markup, reply_to_message_id=message.message_id)
+        await client.send_sticker(chat_id=message.chat.id, sticker='CAACAgUAAxkBAAEEKE5iLyYW69uDoZVfqeBC0CNcdI30XAACmAUAAl4dgFXmib0IFBTHliME', reply_markup=reply_markup, reply_to_message_id=message.message_id)
         await asyncio.sleep(60)
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
